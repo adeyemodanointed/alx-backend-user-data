28b255d69c9174d36b21b2fd30e30d4803230549
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-05-02 22:53:03 +0000

    auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Auth class"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Auth Class"""
+    def __init__(self):
+        pass
+
+
+    def require_auth(
+            self, 
+            path: str, 
+            excluded_paths: List[str]) -> bool:
+        """Check paths that require auth"""
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """Authorization header method"""
+        self.request = request
+        return None
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Checks for current user"""
+        self.request = request
+        return None
