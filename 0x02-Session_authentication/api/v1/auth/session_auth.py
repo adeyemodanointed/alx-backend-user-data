64bf7244789da485e1d7f1f8564e7d3478128c6f
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-05-06 22:44:31 +0000

    task 6

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """Session Auth module"""
 from api.v1.auth.auth import Auth
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -26,5 +27,10 @@ class SessionAuth(Auth):
         """returns userId based on sessionId"""
         if session_id is None or type(session_id) is not str:
             return None
-
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """Overload function for current user"""
+        cookie = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(cookie)
+        return User.get(user_id)
