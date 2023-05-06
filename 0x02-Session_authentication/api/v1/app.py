253bc431216aa186951916b266d0be9a5127fb81
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-05-06 13:34:48 +0000

    empty session

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -21,7 +21,7 @@ if (auth == "auth"):
     auth = Auth()
 elif (auth == "basic_auth"):
     auth = BasicAuth()
-elif auth == "session_auth"):
+elif auth == "session_auth":
     auth = SessionAuth()
 
 
