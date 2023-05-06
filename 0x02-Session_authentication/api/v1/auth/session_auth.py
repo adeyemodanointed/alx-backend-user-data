ffabae689d2b2ae288299bbdea68d87cbf01c336
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-05-06 13:30:08 +0000

    empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Session Auth module"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    def __init__(self):
+        """Initialization"""
+        pass
