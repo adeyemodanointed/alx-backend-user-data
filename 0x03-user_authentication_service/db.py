a176ab13b72d129ad1af0b2b0f204cd51eb6db6d
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-05-11 13:03:16 +0000

    straight session

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,7 +34,6 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
         """Adds a user"""
         user = User(email=email, hashed_password=hashed_password)
-        session = self._session
-        session.add(user)
-        session.commit()
+        self._session.add(user)
+        self._session.commit()
         return user
