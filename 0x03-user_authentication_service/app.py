e8af33e9ec71c81788ef21bebcef53fd964377ff
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-05-12 03:39:19 +0000

    task 6

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python3
 """App Entry point"""
+import flask
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Entry point"""
+    return flask.jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
