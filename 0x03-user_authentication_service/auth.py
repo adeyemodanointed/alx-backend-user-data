6d94eb7552dd31cf806d5e44f85a77edbc6dda02
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-05-12 12:38:24 +0000

    hash_password change

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -10,7 +10,7 @@ from uuid import uuid4
 def _hash_password(password: str) -> bytes:
     """Hashed the password and returns a byte"""
     return bcrypt.hashpw(
-            bytes(password, 'utf-8'),
+            password.encode('utf-8'),
             bcrypt.gensalt())
 
 
