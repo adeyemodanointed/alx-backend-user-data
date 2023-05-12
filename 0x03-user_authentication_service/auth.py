3b928703aab79b203e5f98f0c263da9d1ab5fcdf
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-05-12 12:40:35 +0000

    change to auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -93,7 +93,7 @@ class Auth:
         """Updates user password with token"""
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            hashed_password = _hash_password(password).decode("utf-8")
+            hashed_password = _hash_password(password)
             self._db.update_user(
                     user.id,
                     hashed_password=hashed_password,
